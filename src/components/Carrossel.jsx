c53386bb9c9a7f6444c7f7208e0ec8818cd9e6ce
Vinícius Dias <diasvinicius0401@gmail.com>
2025-05-22 23:21:23 -0300

    Style: ajusta style do carrossel

--- src/components/Carrossel.jsx
+++ src/components/Carrossel.jsx
@@ -1,4 +1,7 @@
 import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+// import "./styles/carrossel.css";
 
 const Carrossel = () => {
   const fotos = [
